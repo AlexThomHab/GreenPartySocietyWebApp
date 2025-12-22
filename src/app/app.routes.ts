08c759203bb8115c3e5542f5762b2ee5d8854aa2
AlexThomHab <alex.haberfield@gmail.com>
2025-12-22 10:55:04 +0000

    added blog and events page
    hardcoded a blog and events page with sample data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import {About} from './components/about/about';
 import {Home} from './components/home/home';
+import {Events} from './components/events/events';
+import {Blog} from './components/blog/blog';
 
 export const routes: Routes = [
   {path: '', component: Home },
-  {path: 'about', component: About }
+  {path: 'about', component: About },
+  {path: 'events', component: Events },
+  {path: 'blog', component: Blog }
+
 ];
