d47fb6fe11e61f181a4cd59567ebfb72c84635cd
AlexThomHab <alex.haberfield@gmail.com>
2025-12-22 11:56:59 +0000

    quick bug fix

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  imports: [
+    RouterLink,
+    RouterModule
+  ]
 })
 export class Home {}
